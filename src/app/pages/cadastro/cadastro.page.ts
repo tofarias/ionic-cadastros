61b258edf08e8af2ef6ed7e05ea612fb04c237a8
IOS SENAC <iossenac@fpal152959.fspoa.br>
2019-08-17 15:17:04 -0300

    Validando formulario

--- src/app/pages/cadastro/cadastro.page.ts
+++ src/app/pages/cadastro/cadastro.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ESTADOS } from '../../estados';
+import { ToastController } from '@ionic/angular';
+
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 @Component({
   selector: 'app-cadastro',
@@ -8,12 +13,60 @@ import { ESTADOS } from '../../estados';
 })
 export class CadastroPage implements OnInit {
   estados: any[]
+  formCadastro: FormGroup
 
-  constructor() {
+  constructor(private toastCtrl: ToastController, private fb: FormBuilder) {
     this.estados = ESTADOS
+    this.montaForm()
    }
 
   ngOnInit() {
+  }
+
+  montaForm()
+  {
+    this.formCadastro = this.fb.group(
+      {
+        nome: ['',Validators.required],
+        email: ['', Validators.compose(
+          [
+            Validators.required,
+            Validators.pattern(emailRegex)
+          ]
+        )],
+        uf: ['', Validators.required],
+        senha: ['', Validators.required],
+        senha2: ['']
+      },
+      {
+        validator: this.matchPasswordValidation
+      }
+    )
+  }
+
+  matchPasswordValidation( ac: AbstractControl ){
+    let password = ac.get('senha').value
+    let passwordConfirm = ac.get('senha2').value
+    
+    if( password != passwordConfirm ){
+      ac.get('senha2').setErrors(
+        { MatchPassword: true }
+      )
+    }else{
+      return null
+    }
+  }
+
+  enviar()
+  {
+    this.toastCtrl.create({
+      message: 'cadastro enviado com sucesso',
+      duration: 3000,
+      color: 'success'
+    }).then(msg => {
+      console.log('Form enviado')
+      msg.present()
+    })
   }
 
 }
